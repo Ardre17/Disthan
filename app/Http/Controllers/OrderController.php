12530c3f0d3c4ecd03899244a5e92ada87961388
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-08-05 05:26:24 +0000

    Etiquetas, formato TOTTUS

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -47,13 +47,13 @@ class OrderController extends Controller
     $height = 105 * 2.83464567;
 
     $pdf = Pdf::loadView('orders.pdf.etiqueta-barra', [
-    'item' => $item,
-    'cantidadPorCaja' => $cpc,
-    'cajas' => $cajas,
-    'sueltas' => $sueltas,
-    'barcode' => $barcode,
-    'codigoMostrado' => $codigoParaEtiqueta,
-])->setPaper([0, 0, $width, $height], 'portrait');
+        'item' => $item,
+        'cantidadPorCaja' => $cpc,
+        'cajas' => $cajas,
+        'sueltas' => $sueltas,
+        'barcode' => $barcode,
+        'codigoMostrado' => $codigoParaEtiqueta,
+    ])->setPaper([0, 0, $width, $height], 'landscape');
 
     return $pdf->stream('etiqueta-' . $item->id . '.pdf');
 }
