683dd7bca9c45d4aa8e780d2b215fb8a14a35c3f
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-08-07 19:27:48 +0000

    Actualiza modulo de pedidos y etiquetas

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -38,7 +38,7 @@ class OrderController extends Controller
     $barcode = null;
 
     if ($codigoParaEtiqueta) {
-        $svg = Code128Generator::generateSvg($codigoParaEtiqueta, 2, 30);
+        $svg = Code128Generator::generateSvg($codigoParaEtiqueta, 2, 120);
         $barcode = 'data:image/svg+xml;base64,' . base64_encode($svg);
     }
 
