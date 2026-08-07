5eb9415b55dc5dfe893f74e8697ccbb671bff2c6
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-08-07 19:31:05 +0000

    Actualiza codigo de barras

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -38,7 +38,7 @@ class OrderController extends Controller
     $barcode = null;
 
     if ($codigoParaEtiqueta) {
-        $svg = Code128Generator::generateSvg($codigoParaEtiqueta, 2, 120);
+        $svg = Code128Generator::generateSvg($codigoParaEtiqueta, 1, 80);
         $barcode = 'data:image/svg+xml;base64,' . base64_encode($svg);
     }
 
