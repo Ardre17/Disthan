91166b1c90d5ecac0ddf98dad4d2566f6e112c7b
Ardre17 <99420094+Ardre17@users.noreply.github.com>
2026-08-12 03:49:01 +0000

    Ajusta codigo de barras EAN13

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -127,7 +127,7 @@ public function etiqueta(OrderDetail $item)
     if ($codigoParaEtiqueta) {
         if ($usaCodigoDeProducto) {
             // Tottus usa EAN/JAN-13 según su software de etiquetado
-            $svg = Ean13Generator::generateSvgMm($codigoParaEtiqueta, 0.34, 12.9);
+            $svg = Ean13Generator::generateSvgMm($codigoParaEtiqueta, 0.50, 23.2);
         } else {
             $svg = Code128Generator::generateSvg($codigoParaEtiqueta, 1.5, 60);
         }
